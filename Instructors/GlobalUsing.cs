d648fa25429240d5d30e21015376dc5dec4ae238
ibrahim samir <ibrahim1903005@gmail.com>
2026-01-20 20:59:59 +0200

    chore(deps): add required packages

---
+++ Instructors/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using Instructors.Models;
+global using Instructors.Repository;
+global using Instructors.ViewModel;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using Instructors.Filters;
+global using System.ComponentModel.DataAnnotations;
