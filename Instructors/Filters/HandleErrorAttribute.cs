828bb0249e05a6897949622a23e85193fbeffc85
ibrahim samir <ibrahim1903005@gmail.com>
2026-01-24 17:21:12 +0200

    feat: add filter HandleErrorAttribute and unique Attribute

---
+++ Instructors/Filters/HandleErrorAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Instructors.Filters
+{
+    public class HandleErrorAttribute : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            //ContentResult contentResult = new ContentResult();
+            //contentResult.Content = $"<h1 style='color:red'>Something went wrong , Please try again later </h1><br/><h3 style='color:blue'>{context.Exception.Message}</h3>";
+            //context.Result = contentResult;
+
+            ViewResult viewResult = new ViewResult();
+            viewResult.ViewName = "Error";
+            context.Result = viewResult;
+           
+        }
+    }
+}
---
+++ Instructors/Models/UniqueAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Instructors.Models
+{
+    public class UniqueAttribute : ValidationAttribute
+    {
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            string  Name=value?.ToString() ?? string.Empty;
+            if(Name is null)
+            {
+                return new ValidationResult("Name cannot be null");
+            }
+            int id= (int)validationContext.ObjectType.GetProperty("Id")!.GetValue(validationContext.ObjectInstance)!;
+            AppDbContext context = new AppDbContext();
+
+
+            Course? crs=context.Courses.FirstOrDefault(c=>(c.Name== Name&&c.Id!=id) );
+
+            if(crs is not null)
+            {
+                return new ValidationResult(ErrorMessage ?? "Must be aunique.");
+            }
+            return ValidationResult.Success;
+
+        }
+
+
+
+
+
+
+
+        //override protected ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        //{
+        //    var context = new AppDbContext();
+        //    var entity = context.Set(validationContext.ObjectType).Find(value);
+        //    if (entity != null)
+        //    {
+        //        return new ValidationResult($"{validationContext.DisplayName} must be unique.");
+        //    }
+        //    return ValidationResult.Success;
+        //}
+    }
+}
